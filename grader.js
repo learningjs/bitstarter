820763c1a3c51893a1c9063e20c6a46dc4885209
learningjs <max.devel.javascript@gmail.com>
2013-07-11 22:08:34 -0300

    grader.js now accept an URL as input

--- grader.js
+++ grader.js
@@ -27,6 +27,9 @@ var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
+var rest = require('restler');
+var URL_DEFAULT = "http://powerful-depths-6513.herokuapp.com/";
+
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
@@ -65,9 +68,20 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+				.option('-u, --url', 'url to check', clone(assertFileExists), URL_DEFAULT)
         .parse(process.argv);
+
+		if (program.url) {
+			rest.get(program.url).on('complete', function(result) {
+				fs.writeFileSync("myFile.html", result);
+				program.file = "myFile.html";
+				//console.log(program.file);
+			});
+		}
+
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
+    fs.writeFileSync("outJson.json", outJson);
     console.log(outJson);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
